67244b301677d428c1d356656332a8a01cee7c8b
Chriss5-2 <christiangiovannixd@gmail.com>
2025-06-19 06:42:50 -0500

    feat(script): Actualizar update_all.sh para que muestre todas las respuestas del terminal y lo guarde en update.log

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -1,49 +1,45 @@
 #!/bin/bash
 
-#echo "Actualizando todos los submódulos..."
-#if git submodule update --remote --recursive; then
-#    echo "Submódulos actualizados correctamente."
-#else
-#    echo "Error al actualizar los submódulos."
-#    exit 1
-#fi
-
 set -e
 
-echo "🔄 Actualizando submódulos al último tag semántico v-* y ejecutando Terraform..."
+Log_Path="../umbrella/reports"
+Log_File="$Log_Path/update.log"
+
+mkdir -p "$Log_Path"
+: > "$Log_File"
+
+echo "🔄 Actualizando submódulos al último tag semántico v-* y ejecutando Terraform..." | tee -a "$Log_File"
 
 SUBMODULES=("compute-repo" "network-repo" "storage-repo")
 
 for MODULE in "${SUBMODULES[@]}"; do
-    echo "📦 Procesando $MODULE..."
+    echo "📦 Procesando $MODULE..." | tee -a "$Log_File"
 
     cd "$MODULE"
     
-    # Obtener tags desde el remoto
-    git fetch --tags
+    git fetch --tags >> "$Log_File" 2>&1
 
-    # Buscar el último tag semántico (v-*)
     LATEST_TAG=$(git tag -l 'v-*' --sort=-v:refname | head -n 1)
 
     if [ -z "$LATEST_TAG" ]; then
-        echo "❌ No hay tags válidos en $MODULE"
+        echo "❌ No hay tags válidos en $MODULE" | tee -a "$Log_File"
         cd ..
         continue
     fi
 
-    echo "✅ Último tag: $LATEST_TAG"
-    git checkout "$LATEST_TAG"
+    echo "✅ Último tag: $LATEST_TAG" | tee -a "$Log_File"
+    git checkout "$LATEST_TAG"  >> "$Log_File" 2>&1
 
     # Ejecutar terraform solo si hay archivos .tf
     if ls *.tf >/dev/null 2>&1; then
-        echo "🚀 Ejecutando Terraform en $MODULE..."
-        terraform init -input=false
-        terraform apply -auto-approve
+        echo "🚀 Ejecutando Terraform en $MODULE..." | tee -a "$Log_File"
+        terraform init -input=false | tee -a "$Log_File"
+        terraform apply -auto-approve | tee -a "$Log_File"
     else
-        echo "⚠️ No hay archivos .tf en $MODULE, saltando Terraform."
+        echo "⚠️ No hay archivos .tf en $MODULE, saltando Terraform." | tee -a "$Log_File"
     fi
 
     cd ..
 done
 
-echo "✅ Todos los submódulos actualizados al último tag semántico"
+echo "✅ Todos los submódulos actualizados al último tag semántico" | tee -a "$Log_File"
