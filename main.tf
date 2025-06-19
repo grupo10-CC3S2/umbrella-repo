0206da81bc1e3876f61c6c8cdaf15fa85f37198e
Chriss5-2 <christiangiovannixd@gmail.com>
2025-06-19 00:11:05 -0500

    feat(main.tf): Añadiendo a main.tf dependencias con directorio

--- main.tf
+++ main.tf
@@ -1,26 +1,22 @@
 module "compute-repo" {
-  # git::https://github.com/tu-usuario/terraform-modulos.git//vpc?ref=v1.0.0
-  # ./compute-repo
   source       = "./compute-repo"
-  name         = var.name
-  network_name = var.network_name
+  instance_count = 4
+  name         = "server-1"
+  network_name = module.network-repo.network_name_out
+  depends_on = [ module.network-repo ]
 }
 
 module "network-repo" {
   source = "./network-repo"
-  name   = var.name
+  name   = "network-1"
   port   = 8080
 }
 
 module "storage-repo" {
   source       = "./storage-repo"
-  name         = var.name
-  server_name  = var.server_name
-  network_name = var.network_name
-}
+  name         = "db-1"
+  server_name  = module.compute-repo.server_name_out
+  network_name = module.compute-repo.network_name_out
 
-resource "null_resource" "umbrella-repo" {
-  provisioner "local-exec" {
-    command = "echo 'Creando umbrella-repo con nombre ${var.name}'"
-  }
+  depends_on = [ module.network-repo, module.compute-repo ]
 }
